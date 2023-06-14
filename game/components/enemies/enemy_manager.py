dd274ac9f92cd7ec6429e692b345996fa21d5a83
LizEspinosa <136389969+LizEspinosa@users.noreply.github.com>
2023-06-14 16:56:07 -0500

    agregar naves
     con temporizador de  tiempo  cada 3  segundos  5 naves consecutivas

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,26 +1,26 @@
-import time
-from game.components.enemies.enemy import Enemy
-
+import time, random
+from game.components.enemies.enemy import Enemy,SCREEN_HEIGHT,SCREEN_WIDTH
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
+        self.last_enemy_time = time.time()
 
-    def update (self):
+    def update(self):
         self.add_enemy()
         for enemy in self.enemies:
             enemy.update(self.enemies)
 
-    def draw (self, screen):
+    def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
 
     def add_enemy(self):
-        if len (self.enemies) < 2:
+        if len(self.enemies) < 5 and time.time() - self.last_enemy_time >= 3:
             enemy = Enemy()
+            enemy.x = random.randint(-SCREEN_WIDTH, SCREEN_WIDTH)  
+            enemy.y = random.randint(-SCREEN_HEIGHT, SCREEN_HEIGHT)  
             self.enemies.append(enemy)
-        while True:
-            len (self.enemies)
-            time.sleep(3)
+            self.last_enemy_time = time.time()
 
-        
+
