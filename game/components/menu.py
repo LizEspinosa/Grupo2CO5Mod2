11dd97ebd37479380b2f981b99e03c3cdaaa7342
LizEspinosa <136389969+LizEspinosa@users.noreply.github.com>
2023-06-16 16:57:04 -0500

    f

--- game/components/menu.py
+++ game/components/menu.py
@@ -12,10 +12,18 @@ class Menu:
         self.text = self.font.render(message, True, (0,0,0))
         self.text_rect = self.text.get_rect()
         self.text_rect.center = (self.HALF_SCREEN_WIDTH, self.HALF_SCREEN_HEIGHT)
+        self.dead_ships = 0
 
     def update(self, game):
         pygame.display.update()
         self.handle_events_on_menu(game)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game.playing = False
+                game.running = False
+                game.dead_ships += self.dead_ships  # Increment the count
+            elif event.type == pygame.KEYDOWN:
+                game.run()
 
     def draw (self, screen ):
         screen.blit(self.text, self.text_rect)
@@ -35,3 +43,6 @@ class Menu:
         self.text = self.font.render(message, True, (0,0,0))
         self.text_rect = self.text.get_rect()
         self.text_rect.center = (self.HALF_SCREEN_WIDTH, self.HALF_SCREEN_HEIGHT)
+
+
+    print("Total dead ships:", game.dead_ships)
